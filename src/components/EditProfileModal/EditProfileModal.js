230b3d7a00a0d1f34131bb82771cdd97c768b756
Lochbird <130000036+Lochbird@users.noreply.github.com>
2024-05-05 04:43:33 -0700

    Add Like/Dislike & Edit Profile Functionality

---
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+export const EditProfileModal = ({ handleCloseModal, handleSubmit }) => {
+  const [values, setValues] = useState({ name: "", avatar: "" });
+  const jwt = localStorage.getItem("jwt");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(values, jwt);
+    handleCloseModal();
+  };
+
+  return (
+    <ModalWithForm
+      title={"Change Profile Data"}
+      onClose={handleCloseModal}
+      onSubmit={onSubmit}
+      buttonText={"Save Changes"}
+      name="edit-profile"
+    >
+      <label>
+        Name *
+        <input
+          className="modal__input"
+          type="name"
+          name="name"
+          minLength="1"
+          maxLength="30"
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Avatar *
+        <input
+          className="modal__input"
+          type="url"
+          name="avatar"
+          minLength="1"
+          onChange={handleChange}
+          required
+        />
+      </label>
+    </ModalWithForm>
+  );
+};
