6a19532699922a4e797fb8b8c646302704c252cc
Lochbird <130000036+Lochbird@users.noreply.github.com>
2024-05-09 21:59:43 -0700

    auth.js Fix

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -2,7 +2,7 @@ import { baseUrl } from "../../utils/utils";
 import { checkResponse } from "../../utils/utils";
 
 const signup = ({ name, avatar, email, password }) => {
-  return fetch(`${baseUrl}/signup`, {
+  const res = fetch(`${baseUrl}/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -13,11 +13,11 @@ const signup = ({ name, avatar, email, password }) => {
       email,
       password,
     }),
-  }).then(checkResponse);
+  }).then(() => checkResponse(res));
 };
 
 const login = ({ email, password }) => {
-  return fetch(`${baseUrl}/signin`, {
+  const res = fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -26,17 +26,17 @@ const login = ({ email, password }) => {
       email,
       password,
     }),
-  }).then(checkResponse);
+  }).then(() => checkResponse(res));
 };
 
 const checkToken = (jwt) => {
-  return fetch(`${baseUrl}/users/me`, {
+  const res = fetch(`${baseUrl}/users/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${jwt}`,
     },
-  }).then(checkResponse);
+  }).then(() => checkResponse(res));
 };
 
 export { signup, login, checkToken };
